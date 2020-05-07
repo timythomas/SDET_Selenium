a9867cee76605876024e3a5eae73fb8b376e8b00
Timy <timy.thomas@wipro.com>
2020-05-07 19:22:37 +0530

    Review comments updated

--- src/test/java/tests/TC008_Olay_Registration_Login_Timy.java
+++ src/test/java/tests/TC008_Olay_Registration_Login_Timy.java
@@ -25,19 +25,19 @@ public class TC008_Olay_Registration_Login_Timy extends BaseClass {
 	}	
 	@Test
 	@Parameters("country")
-	public void inValidLogin() throws InterruptedException, ParseException, IOException, org.json.simple.parser.ParseException {
-		LoginPage.loginLanguage("UK");
+	public void inValidLogin(String country) throws InterruptedException, ParseException, IOException, org.json.simple.parser.ParseException {
+		LoginPage.loginLanguage(country);
 		HashMap<String,String> regVals= new HashMap<String, String>();
 		regVals.put("EmailAddress", "tia12@gmail.com");
 		regVals.put("Password", "Wipro@123");
-		LoginPage.userLogin(regVals,"UK");
+		LoginPage.userLogin(regVals,country);
 		LoginPage.verifyInvalidLogin("The email and password combination you entered is incorrect. Please try again, or click the link below to create an account.");
 	}
 	
 	@Test
 	@Parameters("country")
-	public void verifyForgotPassword() throws InterruptedException, ParseException, IOException, org.json.simple.parser.ParseException {
-		LoginPage.loginLanguage("UK");
+	public void verifyForgotPassword(String country) throws InterruptedException, ParseException, IOException, org.json.simple.parser.ParseException {
+		LoginPage.loginLanguage(country);
 		HashMap<String,String> regVals= new HashMap<String, String>();
 		regVals.put("EmailAddress", "ria@gmail.com");
 		LoginPage.verifyforgotPwd(regVals, "We have sent an email to your email address,");
